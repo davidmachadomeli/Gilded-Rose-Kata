c70143cbb93ce142e8a850415545b2bf4b928c6d
David Machado <david.machado@mercadolibre.com>
2017-10-07 22:55:58 -0300

    adding sulfuras category

---
+++ Java/src/main/java/com/gildedrose/SulfurasCategory.java
@@ -0,0 +1,14 @@
+package com.gildedrose;
+
+public class SulfurasCategory implements ItemCategory {
+
+    @Override
+    public void updateQuality(Item item) { }
+
+    @Override
+    public void updateSellIn(Item item) { }
+
+    @Override
+    public void updateExpired(Item item) { }
+
+}
