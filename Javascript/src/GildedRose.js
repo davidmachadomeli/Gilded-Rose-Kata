48f9a03283f3092c675d813250d58d8150814bcb
Agustin Castiarena <castiarena@gmail.com>
2017-11-03 14:39:12 -0300

    Change logic GildedRose

--- Javascript/src/GildedRose.js
+++ Javascript/src/GildedRose.js
@@ -1,92 +1,20 @@
+import SellInUpdater from './SellInUpdater';
+import QualityUpdater from './QualityUpdater';
+
 class GildedRose {
   constructor(items) {
-    this.PASS_SELL_IN_FIRST_LIMIT = 10;
-    this.PASS_SELL_IN_SECOND_LIMIT = 5;
     this.items = items;
   }
 
   updateQuality() {
-    this.items.forEach(item => this.updateQualityForOneItem(item));
-  }
+    const sellInUpdater = new SellInUpdater(this.items);
+    const qualityUpdater = new QualityUpdater(sellIn);
 
-  updateQualityForOneItem(item) {
-    this.updateQualityOfItem(item);
-    this.updateSellInOfItem(item);
-
-    if (this.hasExpired(item)) {
-      this.updateQualityOfExpiredItem(item);
-    }
-  }
-
-  updateQualityOfItem(item) {
-    switch (item.name) {
-      case 'Sulfuras, Hand of Ragnaros':
-        break;
-
-      case 'Aged Brie':
-        this.increaseQuality(item);
-        break;
-
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        this.increaseQuality(item);
-
-        if (item.sellIn <= this.PASS_SELL_IN_FIRST_LIMIT) {
-          this.increaseQuality(item);
-        }
-
-        if (item.sellIn <= this.PASS_SELL_IN_SECOND_LIMIT) {
-          this.increaseQuality(item);
-        }
-        break;
-
-      default:
-        this.decreaseQuality(item);
-        break;
-      }
-  }
-
-  updateQualityOfExpiredItem(expiredItem) {
-    switch (expiredItem.name) {
-      case 'Sulfuras, Hand of Ragnaros':
-        break;
-
-      case 'Aged Brie':
-        this.increaseQuality(expiredItem);
-        break;
-
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        expiredItem.quality = 0;
-        break;
-
-      default:
-        this.decreaseQuality(expiredItem);
-        break;
-    }
-  }
-
-  updateSellInOfItem(item) {
-    switch (item.name) {
-      case 'Sulfuras, Hand of Ragnaros':
-        break;
-
-      case 'Aged Brie':
-      case 'Backstage passes to a TAFKAL80ETC concert':
-      default:
-        item.sellIn = item.sellIn - 1;
-        break;
-    }
-  }
-
-  increaseQuality(item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-  }
-
-  decreaseQuality(item) {
-    if (item.quality > 0) {
-      item.quality = item.quality - 1;
-    }
+    sellInUpdater.update();
+    qualityUpdater.update(this.items);
+    qualityUpdater.updateExpired(
+      this.items.filter(item => this.hasExpired(item))
+    );
   }
 
   hasExpired(item) {
@@ -98,7 +26,7 @@ class GildedRose {
 
     this.items.forEach((item) => {
       string += item.toString();
-      string += '/n';
+      string += '\n';
     });
 
     return string;
