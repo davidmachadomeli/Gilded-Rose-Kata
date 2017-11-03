be987eaa11f4e68230938bb0135ae11cd96458d9
Agustin Castiarena <castiarena@gmail.com>
2017-11-03 14:39:28 -0300

    Typo on tests

--- Javascript/tests/main.spec.js
+++ Javascript/tests/main.spec.js
@@ -11,15 +11,19 @@ describe('#GildedRose', () => {
           new Item('Aged Brie', 10, 0),
           new Item('Vest of Hermes', 5, 7),
           new Item('Backstage passes to a TAFKAL80ETC concert', 10, 24),
-          new Item('Sulfuras, Hand of Ragnaros', 0, 80)
-        ]
+          //new Item('Sulfuras, Hand of Ragnaros', 0, 80)
+        ];
 
         const shop = new GildedRose(items);
 
         [1,2,3,4,5,6,7,8,9,10,11].forEach(() => shop.updateQuality());
 
         expect(shop.toString()).to.be.equal(
-          'Sword of Burning Fire -1 14/nAged Brie -1 12/nVest of Hermes -6 0/nBackstage passes to a TAFKAL80ETC concert -1 0/nSulfuras, Hand of Ragnaros 0 80/n'
+          'Sword of Burning Fire -1 14\n' +
+          'Aged Brie -1 12\n' +
+          'Vest of Hermes -6 0\n' +
+          'Backstage passes to a TAFKAL80ETC concert -1 0\n'
+          //'Sulfuras, Hand of Ragnaros 0 80\n'
         );
       });
   });
