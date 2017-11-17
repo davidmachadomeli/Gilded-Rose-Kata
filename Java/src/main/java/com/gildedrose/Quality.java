a83c7bf113746b68b41d4195de3321d287d9259f
Nelson Puglisi <nelson.puglisi@mercadolibre.com>
2017-11-17 13:29:05 -0300

    refactor increase y decrease

--- Java/src/main/java/com/gildedrose/Quality.java
+++ Java/src/main/java/com/gildedrose/Quality.java
@@ -3,23 +3,21 @@ package com.gildedrose;
 public abstract class Quality {
     abstract void update(Item item);
 
-    protected void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
-        }
-    }
-
-    protected void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality--;
-        }
-    }
-
     protected void updateSellIn(Item item) {
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
             expires(item);
         }
+    }
+
+    protected void increaseQuality(Item item) {
+        item.quality++;
+        item.quality = Math.min(50, item.quality);
+    }
+
+    protected void decreaseQuality(Item item) {
+        item.quality--;
+        item.quality = Math.max(0, item.quality);
     }
 
     protected abstract void expires(Item item);
